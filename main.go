6137c4fa0c944dbadf255179cae17efd94aa8621
Henry Akmann <akmann.henry@gmail.com>
2019-12-26 04:41:26 +0100

    edited process restructure and added individual filename output

--- main.go
+++ main.go
@@ -28,43 +28,49 @@ func main() {
   // create pdf object
   pdf := gofpdf.New("P", "mm", "A4", "")
   tr := pdf.UnicodeTranslatorFromDescriptor("")
+	pdf.SetLeftMargin(20)
   pdf.AddPage()
 
   // set header
   pdf.SetFont("Arial", "B", 20)
-  pdf.CellFormat(180, 10, tr(template[0]), borderMode, 1, "L", false, 0, "")
+  pdf.CellFormat(170, 10, tr(template[0]), borderMode, 1, "L", false, 0, "")
 
   // set subheaders
   pdf.SetFont("Arial", "", 12)
-  pdf.MultiCell(180, 6, tr(template[1]), borderMode, "L", false)
+  pdf.MultiCell(170, 6, tr(template[1]), borderMode, "L", false)
   pdf.Ln(6)
 
   // set address
 	xBeforeAddress := pdf.GetX()
 	yBeforeAddress := pdf.GetY()
   pdf.SetFont("Arial", "", 10)
-  pdf.CellFormat(120, 5, tr(template[2]), borderMode, 0, "L", false, 0, "")
+  pdf.CellFormat(110, 5, tr(template[2]), borderMode, 0, "L", false, 0, "")
 
   // set contact
   pdf.MultiCell(60, 5, tr(template[3]), borderMode, "R", false)
   pdf.Ln(16)
 
-
-  // set invoice metadata
-	pdf.SetFont("Arial", "B", 10)
+	// set customer address (INPUT)
+	pdf.SetFont("Arial", "", 10)
   xBeforeMeta := pdf.GetX()
 	yBeforeMeta := pdf.GetY()
-	pdf.Line(xBeforeMeta, yBeforeMeta, xBeforeMeta + 180, yBeforeMeta)
-	pdf.CellFormat(120, 10, tr(input[1]), borderMode, 0, "L", false, 0, "")
+	pdf.SetXY(xBeforeAddress, yBeforeAddress + 10)
+  pdf.MultiCell(60, 5, tr(input[0]), borderMode, "L", false)
+	pdf.SetXY(xBeforeMeta, yBeforeMeta)
+
+  // set invoice metadata (INPUT)
+	pdf.SetFont("Arial", "B", 10)
+	pdf.Line(xBeforeMeta, yBeforeMeta, xBeforeMeta + 170, yBeforeMeta)
+	pdf.CellFormat(110, 10, tr(input[1]), borderMode, 0, "L", false, 0, "")
   pdf.CellFormat(60, 10, tr(input[2]), borderMode, 0, "L", false, 0, "")
   pdf.Ln(12)
 
   // set invoice text1
 	pdf.SetFont("Arial", "", 10)
-  pdf.MultiCell(180, 5, tr(template[4]), borderMode, "L", false)
+  pdf.MultiCell(170, 5, tr(template[4]), borderMode, "L", false)
   pdf.Ln(6)
 
-	// set invoice table
+	// set invoice table (INPUT)
 	var total float64 = 0
 	headerBool := true
 	lines := strings.Split(string(input[3]), "\n")
@@ -72,7 +78,7 @@ func main() {
 		items := strings.Split(string(line), ";")
 		if (headerBool == true) {
 	  	pdf.SetFont("Arial", "B", 12)
-			pdf.CellFormat(100, 6, tr(items[0]), "1", 0, "L", false, 0, "")
+			pdf.CellFormat(90, 6, tr(items[0]), "1", 0, "L", false, 0, "")
 			pdf.CellFormat(20, 6, tr(items[1]), "1", 0, "C", false, 0, "")
 			pdf.CellFormat(20, 6, tr(items[2]), "1", 0, "C", false, 0, "")
 			pdf.CellFormat(20, 6, tr(items[3]), "1", 0, "C", false, 0, "")
@@ -80,7 +86,7 @@ func main() {
 			headerBool = false
 		} else {
 	  	pdf.SetFont("Arial", "", 10)
-			pdf.CellFormat(100, 6, tr(items[0]), "1", 0, "L", false, 0, "")
+			pdf.CellFormat(90, 6, tr(items[0]), "1", 0, "L", false, 0, "")
 			pdf.CellFormat(20, 6, tr(items[1]), "1", 0, "C", false, 0, "")
 			pdf.CellFormat(20, 6, tr(items[2]), "1", 0, "L", false, 0, "")
 			pdf.CellFormat(20, 6, tr(items[3] + " €"), "1", 0, "C", false, 0, "")
@@ -93,7 +99,7 @@ func main() {
 		}
 	}
 	pdf.SetFont("Arial", "B", 10)
-	pdf.CellFormat(100, 6, tr(""), "1", 0, "L", false, 0, "")
+	pdf.CellFormat(90, 6, tr(""), "1", 0, "L", false, 0, "")
 	pdf.CellFormat(20, 6, tr(""), "1", 0, "C", false, 0, "")
 	pdf.CellFormat(20, 6, tr(""), "1", 0, "L", false, 0, "")
 	pdf.CellFormat(20, 6, tr(""), "1", 0, "C", false, 0, "")
@@ -103,29 +109,26 @@ func main() {
 
 	// set invoice text2
 	pdf.SetFont("Arial", "", 10)
-  pdf.MultiCell(180, 5, tr(template[5]), borderMode, "L", false)
+  pdf.MultiCell(170, 5, tr(template[5]), borderMode, "L", false)
 	pdf.Ln(6)
 
 	// set footer data 1 & 2
   pdf.SetFont("Arial", "", 8)
 	xBeforeFooter := pdf.GetX()
 	yBeforeFooter := pdf.GetY()
-  pdf.MultiCell(120, 5, tr(template[6]), borderMode, "L", false)
-	pdf.SetXY(xBeforeFooter + 120, yBeforeFooter)
-  pdf.MultiCell(60, 5, tr(template[7]), borderMode, "L", false)
-
-	// set customer address
-	pdf.SetFont("Arial", "", 10)
-	pdf.SetXY(xBeforeAddress, yBeforeAddress + 10)
-  pdf.MultiCell(60, 5, tr(input[0]), borderMode, "L", false)
+  pdf.MultiCell(115, 5, tr(template[6]), borderMode, "L", false)
+	pdf.SetXY(xBeforeFooter + 115, yBeforeFooter)
+  pdf.MultiCell(55, 5, tr(template[7]), borderMode, "L", false)
 
   // output
-  pdf.OutputFileAndClose("invoice.pdf")
+	path := strings.Replace(tr(input[1]), " ", "_", -1) + ".pdf"
+	log.Printf("Ouput: %v\n", path)
+	pdf.OutputFileAndClose(path)
   log.Println("Invoice generated.")
 }
 
-func getData(file string) ([]string) {
-	content, _ := ioutil.ReadFile(file)
+func getData(path string) ([]string) {
+	content, _ := ioutil.ReadFile(path)
 	result := strings.Split(string(content), "\n;\n")
 	return result
 }
