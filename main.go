f65912a1003ab06efbd2bc92370c689dbc8ad979
Henry Akmann <akmann.henry@gmail.com>
2019-12-27 19:56:21 +0100

    changed input to data

--- main.go
+++ main.go
@@ -9,21 +9,21 @@ import (
 )
 
 const (
-  devMode bool = true
+  devMode bool = false
 )
 
 func main() {
   log.Println("Generating Invoice...")
 
-  // load input & template
+  // load data & template
   template := getData("template.txt")
-  input := getData("input.txt")
+  data := getData("data.txt")
 
   // check debug mode
   borderMode := "0"
   if (devMode == true) {
 	  template = getData("template.txt.dev")
-	  input = getData("input.txt.dev")
+	  data = getData("data.txt.dev")
     borderMode = "1"
   }
 
@@ -52,19 +52,19 @@ func main() {
   pdf.MultiCell(60, 5, tr(template[3]), borderMode, "R", false)
   pdf.Ln(16)
 
-	// set customer address (INPUT)
+	// set customer address (DATA)
 	pdf.SetFont("Arial", "", 10)
   xBeforeMeta := pdf.GetX()
 	yBeforeMeta := pdf.GetY()
 	pdf.SetXY(xBeforeAddress, yBeforeAddress + 10)
-  pdf.MultiCell(60, 5, tr(input[0]), borderMode, "L", false)
+  pdf.MultiCell(60, 5, tr(data[0]), borderMode, "L", false)
 	pdf.SetXY(xBeforeMeta, yBeforeMeta)
 
-  // set invoice metadata (INPUT)
+  // set invoice metadata (DATA)
 	pdf.SetFont("Arial", "B", 10)
 	pdf.Line(xBeforeMeta, yBeforeMeta, xBeforeMeta + 170, yBeforeMeta)
-	pdf.CellFormat(110, 10, tr(input[1]), borderMode, 0, "L", false, 0, "")
-  pdf.CellFormat(60, 10, tr(input[2]), borderMode, 0, "R", false, 0, "")
+	pdf.CellFormat(110, 10, tr(data[1]), borderMode, 0, "L", false, 0, "")
+  pdf.CellFormat(60, 10, tr(data[2]), borderMode, 0, "R", false, 0, "")
   pdf.Ln(12)
 
   // set invoice text1
@@ -72,10 +72,10 @@ func main() {
   pdf.MultiCell(170, 5, tr(template[4]), borderMode, "L", false)
   pdf.Ln(6)
 
-	// set invoice table (INPUT)
+	// set invoice table (DATA)
 	var total float64 = 0
 	headerBool := true
-	lines := strings.Split(string(input[3]), "\n")
+	lines := strings.Split(string(data[3]), "\n")
 	for _, line := range lines {
 		items := strings.Split(string(line), ";")
 		if (headerBool == true) {
@@ -123,7 +123,7 @@ func main() {
   pdf.MultiCell(55, 5, tr(template[7]), borderMode, "L", false)
 
   // output
-	path := strings.Replace(tr(input[1]), " ", "_", -1) + ".pdf"
+	path := strings.Replace(tr(data[1]), " ", "_", -1) + ".pdf"
 	log.Printf("Ouput: %v\n", path)
 	pdf.OutputFileAndClose(path)
   log.Println("Invoice generated.")
